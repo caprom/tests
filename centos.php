cc31a06d51b617eeb40a94ef318cd664ae690237
caprom <omroncenter@yandex.ru>
2018-04-13 13:07:04 +0300

    test2 commit centos1

---
+++ centos.php
@@ -0,0 +1,13 @@
+//*******test2
+<?php
+echo "<p>Current User ID: ".getmyuid()."</p>";
+echo "<p>Current Group ID: ".getmygid()."</p>";
+echo "<p>Current PHP Process ID: ".getmypid()."</p>";
+echo "<p>Current Script Owner: ".get_current_user()."</p>";
+echo "<h1>Hello World!</h1>";
+echo "<p>Current User ID is: ". posix_getuid();
+echo "<p>Current Group ID is: ". posix_getgid();
+echo "</p>"."=========="."</p>";
+echo "</p>".exec('whoami')."</p>";
+echo "</p>".system('id')."</p>";
+?>
