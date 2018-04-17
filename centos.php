17425bb59542486200d9d3eef6f4511b92c7507e
caprom <omroncenter@yandex.ru>
2018-04-17 10:35:56 +0300

    12345

--- centos.php
+++ centos.php
@@ -5,7 +5,7 @@ echo "<p>Current Group ID: ".getmygid()."</p>";
 echo "<p>Current PHP Process ID: ".getmypid()."</p>";
 echo "<p>Current Script Owner: ".get_current_user()."</p>";
 echo "<h1>Hello World!</h1>";
-echo "<h1>Hello World!2-3</h1>";
+echo "<h1>Hello World!2-6</h1>";
 echo "<p>Current User ID is: ". posix_getuid();
 echo "<p>Current Group ID is: ". posix_getgid();
 echo "</p>"."=========="."</p>";
